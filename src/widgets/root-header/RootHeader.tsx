af93694908108970292db510d35b6cfc30364283
Igor Barzakh <igorbarzakh@gmail.com>
2026-04-13 17:53:18 +0300

    feat: add RootHeader component with Logo and Sign in button

---
+++ src/widgets/root-header/RootHeader.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/shared/ui/button";
+import { Logo } from "@/shared/ui/logo";
+
+interface RootHeaderProps {
+  onSignInClick?: () => void;
+}
+
+export function RootHeader({ onSignInClick }: RootHeaderProps) {
+  return (
+    <header className="absolute top-2 left-0 right-0 h-14  flex items-center justify-between px-8 shrink-0">
+      <Logo size="md" />
+
+      <Button variant="outline" size="lg" onClick={onSignInClick}>
+        Sign in
+      </Button>
+    </header>
+  );
+}
