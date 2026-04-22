fce0ecaf5bc7182bc9abd93e974b59ca99c4cb11
Igor Barzakh <igorbarzakh@gmail.com>
2026-04-22 20:01:18 +0300

    chore(query): add tanstack query devtools

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,6 +1,18 @@
 'use client'
 
-import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__?: QueryClient
+  }
+}
 
 function makeQueryClient() {
   return new QueryClient({
@@ -15,6 +27,7 @@ function makeQueryClient() {
 }
 
 let browserQueryClient: QueryClient | undefined
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 function getQueryClient() {
   if (isServer) {
@@ -28,9 +41,16 @@ function getQueryClient() {
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient()
 
+  useEffect(() => {
+    if (!isDevelopment) return
+    window.__TANSTACK_QUERY_CLIENT__ = queryClient
+  }, [queryClient])
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+
+      {isDevelopment && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   )
 }
