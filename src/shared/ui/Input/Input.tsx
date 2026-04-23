7242221410d07623bcfafd0cc37911dccb2754eb
Igor Barzakh <igorbarzakh@gmail.com>
2026-04-23 17:24:59 +0300

    refactor(input): format Input component props for improved readability

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -2,7 +2,11 @@ import * as React from 'react'
 import { Input as InputPrimitive } from '@base-ui/react/input'
 import styles from './Input.module.scss'
 
-export function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+export function Input({
+  className,
+  type,
+  ...props
+}: React.ComponentProps<'input'>) {
   return (
     <InputPrimitive
       type={type}
